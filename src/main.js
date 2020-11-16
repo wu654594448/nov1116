62d99c79ca75058a72ac3576f6a10aa617fda3b3
itheima <chen@itcast.cn>
2020-11-16 11:26:31 +0800

    yi

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 import Vue from 'vue'
+import Vant from 'vant'
 import App from './App.vue'
-// import router from './router'
+import router from './router'
 import store from './store/store'
+import 'vant/lib/index.css'
+import 'amfe-flexible'
 
 // 加载全局样式
 import './style/index.less'
 Vue.config.productionTip = false
 
+Vue.use(Vant)
 new Vue({
-  // router,
+  router,
   store,
   render: h => h(App)
 }).$mount('#app')
