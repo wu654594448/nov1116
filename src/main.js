e90c41254aed8b967078b24573320d2f25a46053
itheima <chen@itcast.cn>
2020-11-16 10:15:54 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+// import router from './router'
+import store from './store/store'
 
+// 加载全局样式
+import './style/index.less'
 Vue.config.productionTip = false
 
 new Vue({
-  router,
+  // router,
   store,
   render: h => h(App)
 }).$mount('#app')
